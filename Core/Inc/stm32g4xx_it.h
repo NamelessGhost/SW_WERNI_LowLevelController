8f93b946268f5b583b426e4d01c87d290c9cc6b1
André Krummenacher <krummenacher.andre@gmail.com>
2024-03-10 11:35:44 +0100

    Configured pin mapping

--- Core/Inc/stm32g4xx_it.h
+++ Core/Inc/stm32g4xx_it.h
@@ -55,7 +55,6 @@ void DebugMon_Handler(void);
 void TIM1_UP_TIM16_IRQHandler(void);
 void TIM1_CC_IRQHandler(void);
 void USART1_IRQHandler(void);
-void EXTI15_10_IRQHandler(void);
 void TIM6_DAC_IRQHandler(void);
 void LPUART1_IRQHandler(void);
 /* USER CODE BEGIN EFP */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
+#include "adc.h"
 #include "usart.h"
 #include "tim.h"
 #include "gpio.h"
@@ -93,6 +94,9 @@ int main(void)
   MX_USART1_UART_Init();
   MX_LPUART1_UART_Init();
   MX_UART5_Init();
+  MX_ADC1_Init();
+  MX_TIM2_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
